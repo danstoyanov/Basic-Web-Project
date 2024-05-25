84cda47fed08eb46e62d1cd62ea45d5616599c7c
Daniel Stoyanov <dan.stoyanov@abv.bg>
2024-05-25 14:30:16 +0300

    - Add to Db
    - added a new node page to the db
    - added htmlspecial char function for preventing SQL injection.
    - updating localhost.

--- views/note-view.php
+++ views/note-view.php
@@ -5,7 +5,7 @@
     <main>
         <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
             <!-- Your content -->
-            <?= $note['body'] ?>
+            <?= htmlspecialchars($note['body']) ?>
             <br>
             <button class="bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                 <a href="/notes">
