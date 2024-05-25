a470d941c7485f9824a572877685bf0f03d2aeb5
Daniel Stoyanov <dan.stoyanov@abv.bg>
2024-05-25 15:37:27 +0300

    - Added validations
    - tested browser side validation adding require attribute in the form.
    - adding server side validation
    - adding in note-create.view warning text.

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -1,15 +1,32 @@
 <?php
 
-$config = require ('config.php');
+$config = require('config.php');
 $db = new Database($config['database']);
 
 $header = "Creating Node";
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $db->query("INSERT INTO notes(body, user_id) VALUES(:body, :user_id)", [
-        'body' => $_POST['body'],
-        'user_id' => 1
-    ]);
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+
+    // Server side validation
+    $errors = [];
+
+    // if input body is empty
+    if (strlen($_POST["body"]) === 0) {
+        $errors['body'] = 'The body field is required.';
+    }
+
+    // check the length of the input body
+    if (strlen($_POST['body']) > 1000) {
+        $errors['body'] = 'The body is too long!!';
+    }
+
+    // if there are no errors, that means the form is correct!
+    if (empty($errors)) {
+        $db->query("INSERT INTO notes(body, user_id) VALUES(:body, :user_id)", [
+            'body' => $_POST['body'],
+            'user_id' => 1
+        ]);
+    }
 }
 
 require "views/note-create.view.php";
